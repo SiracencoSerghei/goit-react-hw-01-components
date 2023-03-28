5c8211ba7227bcf131ecc7473646a996f2b07ec1
Siracenco Serghei <siracencoserghei@gmail.com>
2023-03-28 21:01:11 +0200

    changes

--- src/components/FriendList/FriendsList.jsx
+++ src/components/FriendList/FriendsList.jsx
@@ -25,14 +25,3 @@ Friends.propTypes = {
 };
 
 export default Friends;
-
-{/* <ul class="friend-list">
-
-    <!-- Произвольное кол-во FriendListItem -->
-</ul> */}
-
-{/* <li class="item">
-    <span class="status"></span>
-    <img class="avatar" src="" alt="User avatar" width="48" />
-    <p class="name"></p>
-</li> */}
