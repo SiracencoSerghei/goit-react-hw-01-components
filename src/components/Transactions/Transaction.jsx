92f81cf9e770af348b1e16ea082cddd5df1ba42c
Siracenco Serghei <siracencoserghei@gmail.com>
2023-03-30 18:47:24 +0200

    add 4th task

--- src/components/Transactions/Transaction.jsx
+++ src/components/Transactions/Transaction.jsx
@@ -1,30 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './Transaction.module.css';
+
+const TransactionHistory = ({items}) => { 
+    // console.log(items);
+    return (<table className={css.transactionHistory}>
+        <thead className={css.thead}>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+        <tbody className={css.tbody}>
+            {items.map(({ id, type, amount, currency }, index) => {
+                return (
+                    <tr key={id} style={{ backgroundColor: index % 2 === 0 ? 'white' : 'aliceblue' }} >
+                    <td>{type}</td>
+                    <td>{amount}</td>
+                    <td>{currency}</td>
+                </tr>
+        )
+        })}
+        </tbody>
+        
+    </table>)
+};
+
+TransactionHistory.propTypes = {
+    items: PropTypes.array
+};
+
+export default TransactionHistory;
 
 
 
-
-
-
-
-
-<table class="transaction-history">
-    <thead>
-        <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <tr>
-            <td>Invoice</td>
-            <td>125</td>
-            <td>USD</td>
-        </tr>
-        <tr>
-            <td>Withdrawal</td>
-            <td>85</td>
-            <td>USD</td>
-        </tr>
-    </tbody>
-</table>
